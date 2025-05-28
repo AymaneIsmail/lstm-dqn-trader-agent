e92771d92d56391b567b712b59205c9ac073ea51
Aymane <aymane@cashflowpositifi.fr>
2025-05-28 16:24:36 +0200

    init

---
+++ download_dataset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Tes identifiants Kaggle (constantes)
+KAGGLE_USERNAME="aymaneismail"
+KAGGLE_KEY="afb11a143ae8c8ca2eb6cff5ae8e582d"
+
+# URL du dataset
+DATASET_URL="https://www.kaggle.com/api/v1/datasets/download/pavankrishnanarne/global-stock-market-2008-present"
+
+mkdir -p data
+
+ZIP_PATH="data/global-stock-market-2008-present.zip"
+
+echo "Téléchargement du dataset Kaggle dans $ZIP_PATH ..."
+
+curl -L -u $KAGGLE_USERNAME:$KAGGLE_KEY -o $ZIP_PATH $DATASET_URL
+
+if [ -f "$ZIP_PATH" ]; then
+    echo "Téléchargement terminé, extraction dans ./data"
+    unzip -o $ZIP_PATH -d data
+else
+    echo "Erreur : le téléchargement a échoué."
+    exit 1
+fi
